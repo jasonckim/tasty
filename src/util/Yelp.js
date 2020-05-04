89430a979f30e35e2322a8c9ff8890651a82dced
jasonckim <jasonckim10@gmail.com>
2020-05-03 20:30:31 -0700

    first commit

---
+++ src/util/Yelp.js
@@ -0,0 +1,30 @@
+const apiKey = 'wMpOHpkExDkEvQWI-nhHs027Le34_XjN4Uis35JudJGCYoNKdLc97nn6pK79ugSSmBtSjcGmWNNAuWmOjeD_k4nkHPWwEuu4ljXGYxZNKkK-f_vWZ1XTswntqm6nXnYx'
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: { Authorization: `Bearer ${apiKey}`}
+      }).then(response => {
+        return response.json()
+      }).then(jsonResponse => {
+        if(jsonResponse.businesses){
+          return jsonResponse.businesses.map(business => ({
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.location.address1,
+              city: business.location.city,
+              state: business.location.state,
+              zipCode: business.location.zip_code,
+              category: business.categories[0].title,
+              rating: business.rating,
+              reviewCount: business.review_count,
+              url: business.url
+            }))
+          }
+        })
+      }
+    }
+
+export default Yelp;
